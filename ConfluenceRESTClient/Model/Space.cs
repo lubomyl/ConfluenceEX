6b2992625c5f4e8faebff26e4a1d3dfe8c4a538f
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-05-08 11:51:40 +0100

    Adding model for spaces resource

---
+++ ConfluenceRESTClient/Model/Space.cs
@@ -0,0 +1,11 @@
+﻿namespace ConfluenceRestClient.Model
+{
+    public class Space
+    {
+
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+
+    }
+}
