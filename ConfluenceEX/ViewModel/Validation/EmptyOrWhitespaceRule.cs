cbb3a0d89c069425c9524da7ee880a6756a27617
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-08-23 14:19:41 +0200

    Implementing sign-in data binding validation (empty, whitespace or null)

---
+++ ConfluenceEX/ViewModel/Validation/EmptyOrWhitespaceRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ConfluenceEX.ViewModel.Validation
+{
+    public class EmptyOrWhitespaceRule : ValidationRule
+    {
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(String.IsNullOrWhiteSpace((string) value) || String.IsNullOrEmpty((string) value))
+            {
+                return new ValidationResult(false, "Cannot be empty");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+
+    }
+}
