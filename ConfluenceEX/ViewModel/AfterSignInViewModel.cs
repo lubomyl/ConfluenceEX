18e4f6df36eb98cd70c251e76a00d419f01d54a3
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-08-27 10:49:34 +0200

    Removing accessToken, accessTokenSecret from User Settings Store on sign-out

--- ConfluenceEX/ViewModel/AfterSignInViewModel.cs
+++ ConfluenceEX/ViewModel/AfterSignInViewModel.cs
@@ -3,6 +3,9 @@ using ConfluenceEX.Common;
 using ConfluenceRestClient.Model;
 using ConfluenceRESTClient.Service;
 using ConfluenceRESTClient.Service.Implementation;
+using Microsoft.VisualStudio.Settings;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Settings;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -17,21 +20,32 @@ namespace ConfluenceEX.ViewModel
 
         private ConfluenceToolWindowNavigatorViewModel _parent;
 
+        private WritableSettingsStore _userSettingsStore;
+
         public DelegateCommand SignOutCommand { get; private set; }
 
         public AfterSignInViewModel(ConfluenceToolWindowNavigatorViewModel parent, string username, string password)
         {
             this._parent = parent;
 
+            SettingsManager settingsManager = new ShellSettingsManager(ServiceProvider.GlobalProvider);
+            this._userSettingsStore = settingsManager.GetWritableSettingsStore(SettingsScope.UserSettings);
+
             this.SignOutCommand = new DelegateCommand(SignOut);
         }
 
         private void SignOut(object parameter)
         {
-            SignedInUser.Username = string.Empty;
-            SignedInUser.Password = string.Empty;
+            this.DeletePropertyFromUserSettings("AccessToken");
+            this.DeletePropertyFromUserSettings("AccessTokenSecret");
 
             this._parent.ShowBeforeSignIn();
+        }
+
+        //TODO refactor extract to Helper class
+        private void DeletePropertyFromUserSettings(string propertyName)
+        {
+            this._userSettingsStore.DeleteProperty("External Tools", propertyName);
         }
 
     }
