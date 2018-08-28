70fb1c2c3434bb48a55ec26c29ba392d88b333d5
lubomyl <31207065+lubomyl@users.noreply.github.com>
2018-08-28 13:02:35 +0200

    Implementing History model class - represents operations with confluence app object (e. g. Page update)

---
+++ ConfluenceRESTClient/Model/History.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfluenceRESTClient.Model
+{
+    public class History
+    {
+
+        public User CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
